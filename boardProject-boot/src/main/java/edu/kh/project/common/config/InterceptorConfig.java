75ef1d8c0568048dcce25564df9318fbba104a9b
NIMGNUOY <ssong9214@gmail.com>
2024-04-29 02:01:36 +0900

    24-04-29

--- boardProject-boot/src/main/java/edu/kh/project/common/config/InterceptorConfig.java
+++ boardProject-boot/src/main/java/edu/kh/project/common/config/InterceptorConfig.java
@@ -11,6 +11,9 @@ import edu.kh.project.common.interceptor.BoardTypeInterceptor;
 
 @Configuration	// 서버가 켜지면 내부 메서드를 모두 수행
 public class InterceptorConfig implements WebMvcConfigurer{
+	// WebMvcConfigurer : Spring MVC 프레임 워크에서 제공하는 인터페이스 중 하나로,
+	//					스프링 구성을 커스터마이징하고 확장하기 위한 메서드를 제공함.
+	// 					주소 웹 어플리케이션의 설정을 조정하거나 추가하는데 사용됨.
 	
 	// 인터셉터 클래스 Bean 등록
 	@Bean	// 개발자가 만들어서 반환하는 객체를 Bean 등록

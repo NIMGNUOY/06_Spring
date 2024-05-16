56dfa77ac1e53941025311cd8b944136a23df548
NIMGNUOY <ssong9214@gmail.com>
2024-05-16 17:05:25 +0900

    240516 sym

--- boardProject-boot/src/main/java/edu/kh/project/email/model/service/EmailServiceImpl.java
+++ boardProject-boot/src/main/java/edu/kh/project/email/model/service/EmailServiceImpl.java
@@ -142,25 +142,25 @@ public class EmailServiceImpl implements EmailService{
 	/** 인증번호 생성 (영어 대문자 + 소문자 + 숫자 6자리)
 	 * @return authKey
 	 */
-	   public String createAuthKey() {
-	   	String key = "";
-	       for(int i=0 ; i< 6 ; i++) {
-	          
-	           int sel1 = (int)(Math.random() * 3); // 0:숫자 / 1,2:영어
-	          
-	           if(sel1 == 0) {
-	              
-	               int num = (int)(Math.random() * 10); // 0~9
-	               key += num;
-	              
-	           }else {
-	              
-	               char ch = (char)(Math.random() * 26 + 65); // A~Z
-	              
-	               int sel2 = (int)(Math.random() * 2); // 0:소문자 / 1:대문자
-	              
-	               if(sel2 == 0) {
-	                   ch = (char)(ch + ('a' - 'A')); // 대문자로 변경
+	public String createAuthKey() {
+	String key = "";
+	   for(int i=0 ; i< 6 ; i++) {
+	      
+	       int sel1 = (int)(Math.random() * 3); // 0:숫자 / 1,2:영어
+	  
+	   if(sel1 == 0) {
+	      
+	       int num = (int)(Math.random() * 10); // 0~9
+	       key += num;
+	      
+	   }else {
+	      
+	       char ch = (char)(Math.random() * 26 + 65); // A~Z
+	  
+	   int sel2 = (int)(Math.random() * 2); // 0:소문자 / 1:대문자
+	  
+	   if(sel2 == 0) {
+	       ch = (char)(ch + ('a' - 'A')); // 대문자로 변경
 	               }
 	              
 	               key += ch;
@@ -168,7 +168,7 @@ public class EmailServiceImpl implements EmailService{
 	          
 	       }
 	       return key;
-	   }
+	}
 
 
 	// 이메일, 인증번호 확인
